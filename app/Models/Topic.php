d08b37d4db029f27e62ad078a0b896f0a3a160d9
jzb1006 <1129990545@qqq.com>
2019-04-21 11:34:00 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,20 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt'];
+    protected $fillable = ['title', 'body',
+        'user_id', 'category_id', 'reply_count',
+        'view_count', 'last_reply_user_id',
+        'order', 'excerpt','slug'];
+
+    //一对一分类模型
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //一对一用户模型
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
 }
