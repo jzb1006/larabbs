1265403e4e67056287dce3d0bbeb8fa299e2908b
jzb1006 <1129990545@qqq.com>
2019-04-23 20:26:03 +0800

    用户可以删除自己的话题

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -37,12 +37,14 @@
                     </div>
                     <div class="operate">
                       <hr>
+                        @can('update',$topic)
                       <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs pull-left" role="button">
                       <i class="far fa-edit"></i> 编辑
-              </a>
+                      </a>
               <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
                 <i class="far fa-trash-alt"></i> 删除
               </a>
+                            @endcan
             </div>
           </div>
         </div>
