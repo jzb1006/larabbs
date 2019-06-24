b0df1787d3848dd1529d242e39621820f0aa47a2
jzb1006 <1129990545@qq.com>
2019-06-24 15:18:03 +0800

    add passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Horizon\Horizon;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,6 +31,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        //passport 的路由
+        Passport::routes();
+        //access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        //refreshTokens 刷新时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        
         Horizon::auth(function ($request){
             //是否是站长
             return Auth::user()->hasRole('Founder');
